4c5c6308f9f9f77fb01c0c4f376800b2f5f402bb
sasaki <y.sasaki.roppongi@gmail.com>
2024-01-16 16:07:10 +0900

    first commit

---
+++ src/Main1.java
@@ -0,0 +1,17 @@
+
+public class Main1 {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+
+		int sum = a + b;
+		int sub = a - b;
+		int div = a / b;
+		int multi = a * b;
+
+		System.out.println("足し算の結果：" + sum);
+		System.out.println("引き算の結果：" + sub);
+		System.out.println("割り算の結果：" + div);
+		System.out.println("掛け算の結果：" + multi);
+	}
+}
